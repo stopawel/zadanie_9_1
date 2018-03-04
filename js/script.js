5c63adddf88d894d82556ba34d52a360fa3726c1
Paweł Stolecki <noigell@gmail.com>
2018-03-04 13:27:26 +0100

    zadanie-9-1

---
+++ js/script.js
@@ -0,0 +1,19 @@
+var triangle1Area = getTriangleArea(10, 15);
+var triangle2Area = getTriangleArea(0, 15);
+var triangle3Area = getTriangleArea(10, 1);
+
+function getTriangleArea(a, h)
+{
+	if ((a > 0) && (h > 0))
+	{
+		return a * h / 2;
+	}
+	else
+	{
+		return 'Nieprawidłowe dane';
+	}
+}
+
+	console.log(triangle1Area);
+	console.log(triangle2Area);
+	console.log(triangle3Area);
