34451e910bf3aa1e476a3267648e3ebd49fc7005
Paweł Stolecki <noigell@gmail.com>
2018-03-05 21:17:28 +0100

    zadanie-9-1 poprawki

--- js/script.js
+++ js/script.js
@@ -2,18 +2,16 @@ var triangle1Area = getTriangleArea(10, 15);
 var triangle2Area = getTriangleArea(0, 15);
 var triangle3Area = getTriangleArea(10, 1);
 
-function getTriangleArea(a, h)
-{
-	if ((a > 0) && (h > 0))
-	{
+function getTriangleArea(a, h) {
+	
+	if ((a > 0) && (h > 0)) {
 		return a * h / 2;
 	}
-	else
-	{
+	else {
 		return 'Nieprawidłowe dane';
 	}
 }
 
-	console.log(triangle1Area);
-	console.log(triangle2Area);
-	console.log(triangle3Area);
+console.log(triangle1Area);
+console.log(triangle2Area);
+console.log(triangle3Area);
